8e53e487dcc506fe5fb83e88921a2ee739db3e13
Ravien Dalpatadu <dalpataduravien@gmail.com>
2022-11-02 02:11:47 +0530

    hosting using heroku attempt 1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const { type } = require("os");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "scources")));
