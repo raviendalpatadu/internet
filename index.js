eb52a1a33523c1bfc38abd45aaa9ea026fb5a650
Ravien Dalpatadu <dalpataduravien@gmail.com>
2022-11-03 16:58:48 +0530

    after changing google.com to http://google.com

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function getNetworkUploadSpeed() {
 }
 
 async function getPing() {
-  var hosts = ["google.com"];
+  var hosts = ["http://google.com"];
   var sum = 0;
   var loss = 0;
   try {
@@ -90,7 +90,7 @@ async function getLocation(req, res) {
 
     let location = axios
       .get(
-        "https://ipgeolocation.abstractapi.com/v1/?api_key=" + API_KEY + "&ip_address=" + ipAddr
+        "https://ipgeolocation.abstractapi.com/v1/?api_key=" + API_KEY //+ "&ip_address=" + ipAddr
       )
       .then((response) => {
         return response.data;
